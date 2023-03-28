69fb09933db5b9fa0c6d2f83bba021dafa89cb12
watase.atsushi <beechan920@yahoo.co.jp>
2023-03-28 10:53:03 +0900

    ヘッダーの表示・非表示の設定

--- react/src/components/Header/Header.tsx
+++ react/src/components/Header/Header.tsx
@@ -1,53 +1,65 @@
+import { useSelector } from "react-redux";
 import { useNavigate } from "react-router";
+import { NavLink } from "react-router-dom";
+import { LoginState } from "../../redux/loginSlice";
+import { RootState } from "../../redux/store";
 
 function Header() {
-  const navigate = useNavigate();
-
-  const signInClick = () => {
-    navigate("/login");
-  };
-
-  const signUpClick = () => {
-    navigate("/register");
-  };
+  const login = useSelector((state: RootState) => state.login);
 
   return (
     <nav className="navbar navbar-light">
-      <div className="container">
+      <div className="container flex items-center justify-between">
         <a className="navbar-brand" href="index.html">
           conduit
         </a>
         <ul className="nav navbar-nav pull-xs-right">
-          <li className="nav-item">
-            <a className="nav-link active" href="">
-              Home
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="">
-              {" "}
-              <i className="ion-compose"></i>&nbsp;New Article{" "}
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="">
-              {" "}
-              <i className="ion-gear-a"></i>&nbsp;Settings{" "}
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" onClick={signInClick}>
-              Sign in
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" onClick={signUpClick}>
-              Sign up
-            </a>
-          </li>
+          <NavItem text="Home" href="/" />
+          {login.loginIn ? <UserLinks login={login} /> : <GuestLinks />}
         </ul>
       </div>
     </nav>
+  );
+}
+
+function NavItem({
+  text,
+  href,
+  icon,
+}: {
+  text: string;
+  href: string;
+  icon?: string;
+}) {
+  return (
+    <li className="nav-item">
+      <NavLink
+        className={({ isActive }) => (isActive ? undefined : "text-gray-400")}
+        to={href}
+      >
+        {icon && <i className={icon}></i>}&nbsp;
+        {text}
+      </NavLink>
+    </li>
+  );
+}
+
+function GuestLinks() {
+  return (
+    <>
+      <NavItem text="Sing In" href="/login" />
+      <NavItem text="Sign up" href="/register" />
+    </>
+  );
+}
+
+function UserLinks({ login: { user } }: { login: LoginState }) {
+  return (
+    <>
+      <NavItem text="New Article" href="/editor" icon="ion-compose" />
+      <NavItem text="Settings" href="/settings" icon="ion-gear-a" />
+      <NavItem text={`${user.username}`} href={`/profile/${user.username}`} />
+    </>
   );
 }
 
