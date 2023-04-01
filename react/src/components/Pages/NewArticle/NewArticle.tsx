8c2e8b9c08c25bf3a663e8805181885ab0d8c5ed
watase.atsushi <beechan920@yahoo.co.jp>
2023-04-01 12:51:53 +0900

    New Article画面の入力処理の実装

--- react/src/components/Pages/NewArticle/NewArticle.tsx
+++ react/src/components/Pages/NewArticle/NewArticle.tsx
@@ -1,4 +1,52 @@
+import { useState } from "react";
+import { ArticleForEditor } from "../../../types/article";
+
 function NewArticle() {
+  const [input, setInput] = useState<ArticleForEditor>({
+    title: "",
+    description: "",
+    body: "",
+    tag: "",
+    tagList: [],
+  });
+
+  const handleChange = (
+    e:
+      | React.ChangeEvent<HTMLInputElement>
+      | React.ChangeEvent<HTMLTextAreaElement>
+  ) => {
+    const { name, value } = e.target;
+    setInput({ ...input, [name]: value });
+  };
+
+  const onSubmit = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    const params = {
+      article: {
+        title: input.title,
+        description: input.description,
+        body: input.body,
+        tagList: input.tagList,
+      },
+    };
+    console.log("params:", params);
+  };
+
+  const addTag = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      if (input.tagList.includes(input.tag) || input.tag === "") {
+        return;
+      }
+      setInput({ ...input, tag: "", tagList: [...input.tagList, input.tag] });
+    }
+  };
+
+  const onRemoveItem = (target: string) => {
+    setInput({
+      ...input,
+      tagList: input.tagList.filter((tag) => tag !== target),
+    });
+  };
+
   return (
     <div className="editor-page">
       <div className="container page">
@@ -11,13 +59,19 @@ function NewArticle() {
                     type="text"
                     className="form-control form-control-lg"
                     placeholder="Article Title"
+                    name="title"
+                    value={input.title}
+                    onChange={handleChange}
                   />
                 </fieldset>
                 <fieldset className="form-group">
                   <input
                     type="text"
+                    value={input.description}
                     className="form-control"
                     placeholder="What's this article about?"
+                    name="description"
+                    onChange={handleChange}
                   />
                 </fieldset>
                 <fieldset className="form-group">
@@ -25,6 +79,9 @@ function NewArticle() {
                     className="form-control"
                     rows={8}
                     placeholder="Write your article (in markdown)"
+                    name="body"
+                    value={input.body}
+                    onChange={handleChange}
                   ></textarea>
                 </fieldset>
                 <fieldset className="form-group">
@@ -32,12 +89,27 @@ function NewArticle() {
                     type="text"
                     className="form-control"
                     placeholder="Enter tags"
+                    name="tag"
+                    value={input.tag}
+                    onChange={handleChange}
+                    onKeyDown={addTag}
                   />
-                  <div className="tag-list"></div>
+                  <div className="tag-list">
+                    {input.tagList.map((value) => (
+                      <span key={value} className="tag-default tag-pill">
+                        <i
+                          className="ion-close-round"
+                          onClick={() => onRemoveItem(value)}
+                        ></i>
+                        {value}
+                      </span>
+                    ))}
+                  </div>
                 </fieldset>
                 <button
                   className="btn btn-lg pull-xs-right btn-primary bg-green-10"
                   type="button"
+                  onClick={onSubmit}
                 >
                   Publish Article
                 </button>
---
+++ react/src/types/article.ts
@@ -0,0 +1,7 @@
+export interface ArticleForEditor {
+  title: string;
+  description: string;
+  body: string;
+  tag: string;
+  tagList: string[];
+}
